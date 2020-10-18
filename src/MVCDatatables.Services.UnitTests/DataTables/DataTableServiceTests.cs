1c60f7f6e8cb6b7efbb50018cf18f882a0af3657
Scott Barton <scottystrike@gmail.com>
2020-10-18 18:02:04 +0100

    - Added unit tests for the datatable service. - Changed the solution folder name.

---
+++ src/MVCDatatables.Services.UnitTests/DataTables/DataTableServiceTests.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mvc.JQuery.DataTables;
+using MVCDatatables.Services.Datatables;
+using NUnit.Framework;
+using Shouldly;
+
+namespace MVCDatatables.Services.UnitTests.DataTables
+{
+    [TestFixture]
+    public class DataTableServiceTests
+    {
+        [Test]
+        public void GetFilteredResultOrDefault_GivenRecordsFilteredResultLessThanOrEqualToZero_ShouldReturnNull()
+        {
+            //Arrange
+            var dataTableService = new DataTableService();
+            var list = new List<string>();
+
+            //Act
+            var result = dataTableService.GetFilteredResultOrDefault(new DataTablesParam(), list.AsQueryable());
+
+            //Assert
+            result.ShouldBeNull();
+        }
+
+        [Test]
+        public void GetFilteredResultOrDefault_GivenRecordsFilteredWithNoFilterResultGreaterThanZero_ShouldReturnThreeRecords()
+        {
+            //Arrange
+            var dataTableService = new DataTableService();
+            var list = new List<string>() { "one", "two", "three" };
+            var param = new DataTablesParam();
+
+            //Act
+            var result = dataTableService.GetFilteredResultOrDefault(param, list.AsQueryable());
+
+            //Assert
+            result.recordsFiltered.ShouldBe(3);
+        }
+
+        [Test]
+        public void GetFilteredResultOrDefault_GivenRecordsFilteredWithFilterClauseResultGreaterThanZero_ShouldReturnOneRecords()
+        {
+            //Arrange
+            var dataTableService = new DataTableService();
+            var list = new List<string>() { "one", "two", "three" };
+            var param = new DataTablesParam();
+
+            //Act
+            var result = dataTableService.GetFilteredResultOrDefault(param, list.AsQueryable(), x => x == "one");
+
+            //Assert
+            result.recordsFiltered.ShouldBe(1);
+        }
+
+        [Test]
+        public void GetFilteredResultOrDefault_GivenRecordsFilteredWithInvalidFilterClauseResultLessThanOrEqualToZero_ShouldReturnNull()
+        {
+            //Arrange
+            var dataTableService = new DataTableService();
+            var list = new List<string>() { "one", "two", "three" };
+            var param = new DataTablesParam();
+
+            //Act
+            var result = dataTableService.GetFilteredResultOrDefault(param, list.AsQueryable(), x => x == "four");
+
+            //Assert
+            result.ShouldBeNull();
+        }
+
+        [Test]
+        public void GetFilteredResultOrDefault_GivenRecordsFilteredWithNoFilterClauseResultGreaterThanZero_ShouldReturnThreeRecords()
+        {
+            //Arrange
+            var dataTableService = new DataTableService();
+            var list = new List<string>() { "one", "two", "three" };
+            var param = new DataTablesParam();
+
+            //Act
+            var result = dataTableService.GetFilteredResultOrDefault(param, list.AsQueryable(), null);
+
+            //Assert
+            result.recordsFiltered.ShouldBe(3);
+        }
+
+        [Test]
+        public void GetFilteredResultOrDefault_GivenRecordsFilteredWithParamsAndFilterClauseResultGreaterThanZero_ShouldReturnOneRecords()
+        {
+            //Arrange
+            var dataTableService = new DataTableService();
+            var list = new List<string>() { "one", "two", "three" };
+            var param = new DataTablesParam()
+            {
+                sSearch = "one"
+            };
+
+            //Act
+            var result = dataTableService.GetFilteredResultOrDefault(param, list.AsQueryable(), x => x == "one");
+
+            //Assert
+            result.recordsFiltered.ShouldBe(1);
+        }
+    }
+
+}
---
+++ src/MVCDatatables.Services.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MVCDatatables.Services.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MVCDatatables.Services.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("533dd45e-4bed-4d41-bbf8-97129a120128")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
